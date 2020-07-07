8d247e5aa6a20b242e2fec060384f73ccb4b05a4
crodrigoquero <crodrigoquero@gmail.com>
2020-07-07 13:47:00 +0100

    * BasicTest.cs: Integration test added
    * UnitTest1.cs:
    * launchSettings.json:
    * Companies.API.Test.Integration.csproj:
    
    * Companies.API.sln: Added new Intergration test project

---
+++ Companies.API.Test.Integration/BasicTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+
+namespace Companies.API.Test.Integration
+{
+    public class BasicTest : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        private readonly WebApplicationFactory<Startup> _factory;
+
+        public BasicTest(WebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Theory]
+        //[InlineData("/")]
+        [InlineData("/api/Company/1")]
+        public async Task GetHttpRequest(string url)
+        {
+            // arrange
+            var ciient = _factory.CreateClient();
+
+            // Act
+            var response = await ciient.GetAsync(url);
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+
+            //[InlineData("/")]
+            //Assert.Equal("text/html; charset=utf-8", response.Content.Headers.ContentType.ToString());
+
+            Assert.Equal("text/json; charset=utf-8", response.Content.Headers.ContentType.ToString());
+        }
+
+    }
+}
---
+++ Companies.API.Test.Integration/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Companies.API.Test.Integration
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
